55ba5b1859628780965567b22ed55fac1c2e7d17
Brayan Espinoza <bjespinozah@gmail.com>
2017-10-11 02:23:35 -0500

    The app can edit movies

--- api/controllers/MoviesController.js
+++ api/controllers/MoviesController.js
@@ -35,6 +35,28 @@ module.exports = {
 				}
 				response.redirect('movies/list');
 			});
+
+			return false;
+		},
+		edit: function(request, response) {
+			Movies.findOne({id: request.params.id}).exec(function(error, movie){
+				if(error) {
+					response.send(500, {error :'Database Error'});
+				}
+				response.view('edit', {movie: movie});
+			});
+		},
+		update: function(request, response) {
+			var title = request.body.title;
+			var body = request.body.body;
+
+			Movies.update({id: request.params.id}, {title: title, body: body}).exec(function(error){
+				if(error) {
+					response.send(500, {error :'Database Error'});
+				}
+				response.redirect('movies/list');
+			});
+
 			return false;
 		}
 };
